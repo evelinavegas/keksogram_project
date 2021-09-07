c7f2d8455bcd4f71516ccfec3f657d541e800612
mary <mari-bahlina@ukr.net>
2021-09-07 15:45:01 +0300

    task1.correction

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 "use strict"
-import {randomMinMax,  randomMess, randomName,  randomAvatar,randomId, randomUrl, randomRandom } from "./util.js"
+import {    randomAvatar, randomId, randomUrl,  randomArray } from "./util.js"
 
 let names = ["Петя", "Галя", "Валя", "Вася", "Нина", "Катя", "Димон", "Алекс", "Саня"]
 let mess = ["Всё отлично!", "В целом всё неплохо. Но не всё.", "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.", "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.", "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.", "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"]
@@ -11,8 +11,8 @@ let idNumb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
 let comm = [{
   id: randomId(25, idNumb),
   avatar:  randomAvatar(6, numb),
-  message:  randomMess(6, mess),
-  name: randomName(9, names) ,
+  message:  randomArray( mess),
+  name: randomArray( names) ,
 }]
 
 let icon =new Array(25)
@@ -23,8 +23,8 @@ let avatars = icon.map(el=>({
     id:randomId(25, idNumb) ,
     description: "Это аватар!",
     url: randomUrl(25, idNumb),
-    likes: randomMinMax(15, 200) ,
-    comments: randomRandom(10, comm),
+    likes: randomMinMaxlusive(15, 200) ,
+    comments: randomArray( comm),
   }
   
 }))
--- js/util.js
+++ js/util.js
@@ -1,14 +1,12 @@
 "use strict";
 
+// export function randomRandom(max){
+//    return Math.floor(Math.random() * max)
+// }
 
-let mess = ["Всё отлично!", "В целом всё неплохо. Но не всё.", "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.", "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.", "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.", "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"]
-export function randomRandom(max){
-   return Math.floor(Math.random() * max)
-}
-
-
-export function randomMinMax(min, max){
-  return Math.random() * (max - min) + min;
+export function randomArray(array){
+  let myArr = array[Math.floor(Math.random() * array.length)];
+  return myArr;
 }
 
 export function randomMinMaxlusive(min, max){
@@ -17,30 +15,32 @@ export function randomMinMaxlusive(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export function randomMess(max, mess){
-  let messRandom = randomRandom(6);
-  let resultMess = mess.slice(messRandom);
-  return resultMess
-}
-export function randomName(max, namess){
-  let nameRandom = randomRandom(9);
-  let resultName = namess.slice(nameRandom);
-  return resultName
-}
+// export function randomMess(mess){
+//   let messRandom = randomRandom(6);
+//   let resultMess = mess.slice(messRandom);
+//   return resultMess
+// }
 
-export function randomId(max, idNumber){
+// export function randomName(namess){
+//   let nameRandom = randomRandom(9);
+//   let resultName = namess.slice(nameRandom);
+//   return resultName
+// }
+
+export function randomId(idNumber){
   let randomNumb = randomMinMaxlusive(1, 25);
   let idRandom = idNumber.slice(randomNumb)
   return idRandom
 }
 
-export function randomUrl(max, urls){
+export function randomUrl(urls){
   let randomUrl = randomMinMaxlusive(1, 25);
   let sliceUrl = urls.slice(randomUrl);
   let url = "photos/" + sliceUrl + ".jpg"
+  return url
 }
 
-export function randomAvatar(min, numbers){
+export function randomAvatar(){
   let numberAvatar = randomMinMaxlusive(1, 6)
   let avatarRandom = "img/avatar-" + numberAvatar + ".svg"
   return avatarRandom
