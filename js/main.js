37bf90757267a33267330246af6478f639107c66
mary <mari-bahlina@ukr.net>
2021-09-09 18:11:21 +0300

    task1

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
 "use strict"
-import {    randomAvatar, randomId, randomUrl,  randomArray } from "./util.js"
+import {    randomAvatar,   randomArray, randomMinMaxInclusive } from "./util.js"
 
 let names = ["Петя", "Галя", "Валя", "Вася", "Нина", "Катя", "Димон", "Алекс", "Саня"]
 let mess = ["Всё отлично!", "В целом всё неплохо. Но не всё.", "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.", "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.", "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.", "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"]
 
 let numb = [1, 2, 3, 4, 5, 6]
 
-let idNumb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
-let comm = [{
-  id: randomId(25, idNumb),
+let comm = new Array(30).fill()
+let mapComm = comm.map((el, index) => ({
+  id: index,
   avatar:  randomAvatar(6, numb),
   message:  randomArray( mess),
   name: randomArray( names) ,
-}]
-
-let icon =new Array(25)
-icon.fill()
-console.log(icon)
-let avatars = icon.map(el=>({
-  desc : {
-    id:randomId(25, idNumb) ,
-    description: "Это аватар!",
-    url: randomUrl(25, idNumb),
-    likes: randomMinMaxlusive(15, 200) ,
-    comments: randomArray( comm),
-  }
-  
 }))
 
 
+let photo =new Array(25)
+photo.fill()
+console.log(photo)
+let avatars = photo.map((el, index) =>({
+  desc : {
+    id: index ,
+    description: "Это аватар!",
+    url: "photos/" + index + ".jpg",
+    likes: randomMinMaxInclusive(15, 200) ,
+    comments: mapComm,
+  }
+  
+}))
+ 
+console.log(avatars)
 
 
--- js/util.js
+++ js/util.js
@@ -9,39 +9,15 @@ export function randomArray(array){
   return myArr;
 }
 
-export function randomMinMaxlusive(min, max){
+export function randomMinMaxInclusive(min, max){
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// export function randomMess(mess){
-//   let messRandom = randomRandom(6);
-//   let resultMess = mess.slice(messRandom);
-//   return resultMess
-// }
-
-// export function randomName(namess){
-//   let nameRandom = randomRandom(9);
-//   let resultName = namess.slice(nameRandom);
-//   return resultName
-// }
-
-export function randomId(idNumber){
-  let randomNumb = randomMinMaxlusive(1, 25);
-  let idRandom = idNumber.slice(randomNumb)
-  return idRandom
-}
-
-export function randomUrl(urls){
-  let randomUrl = randomMinMaxlusive(1, 25);
-  let sliceUrl = urls.slice(randomUrl);
-  let url = "photos/" + sliceUrl + ".jpg"
-  return url
-}
 
 export function randomAvatar(){
-  let numberAvatar = randomMinMaxlusive(1, 6)
+  let numberAvatar = randomMinMaxInclusive(1, 6)
   let avatarRandom = "img/avatar-" + numberAvatar + ".svg"
   return avatarRandom
 }
