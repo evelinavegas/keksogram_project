0dab5e2d77b61fa3f5cc4aae64b9c2b62bee6da2
mary <mari-bahlina@ukr.net>
2021-09-10 18:41:17 +0300

    task3

--- js/main.js
+++ js/main.js
@@ -1,35 +1,6 @@
-"use strict"
-import {    randomAvatar,   randomArray, randomMinMaxInclusive } from "./util.js"
+import  {avatars} from "./date.js"
+import {clonPicture} from "./miniatures.js"
 
-let names = ["Петя", "Галя", "Валя", "Вася", "Нина", "Катя", "Димон", "Алекс", "Саня"]
-let mess = ["Всё отлично!", "В целом всё неплохо. Но не всё.", "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.", "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.", "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.", "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"]
+let blockPictures = document.querySelector(".pictures")
 
-let numb = [1, 2, 3, 4, 5, 6]
-
-
-let comm = new Array(30).fill()
-let mapComm = comm.map((el, index) => ({
-  id: index,
-  avatar:  randomAvatar(6, numb),
-  message:  randomArray( mess),
-  name: randomArray( names) ,
-}))
-
-
-let photo =new Array(25)
-photo.fill()
-console.log(photo)
-let avatars = photo.map((el, index) =>({
-  desc : {
-    id: index ,
-    description: "Это аватар!",
-    url: "photos/" + index + ".jpg",
-    likes: randomMinMaxInclusive(15, 200) ,
-    comments: mapComm,
-  }
-  
-}))
- 
-console.log(avatars)
-
-
+let fragment = document.createDocumentFragment();
---
+++ js/miniatures.js
@@ -0,0 +1,29 @@
+import {avatars} from "./date.js"
+
+let one = avatars[0]
+
+export function clonPicture(incomingValue){
+  const picture = document.getElementById("picture")
+  .content
+
+  const clonedPicture = picture.cloneNode(true);
+
+  let pictureLikes= clonedPicture.querySelector(".picture__likes")
+  let picturesComments = clonedPicture.querySelector(".picture__comments")
+  let imgSrc = clonedPicture.querySelector(".picture__img")
+  
+
+  let someLikes = incomingValue["desc"].likes
+  pictureLikes.innerText = someLikes
+
+  let someComments = incomingValue["desc"].comments
+  picturesComments.innerText = someComments
+
+  let newUrl = incomingValue["desc"].url
+  debugger
+  `<img src="${newUrl}">`
+ console.log(incomingValue)
+ return clonedPicture
+}
+
+clonPicture(one)
