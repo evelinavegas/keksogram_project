916bd2ce32b650f20d24da6d18e5262509cbb147
mary <mari-bahlina@ukr.net>
2021-10-01 22:17:18 +0300

    task5 valid

--- js/valid.js
+++ js/valid.js
@@ -3,7 +3,6 @@
 const uploadFile = document.getElementById("upload-file")
 const blockOverlay = document.querySelector(".img-upload__overlay ")
 const body = document.querySelector("body")
-const uploadCancel = document.getElementById("upload-cancel")
 const form = document.querySelector(".img-upload__form")
 const hashtagsText = document.querySelector(".text__hashtags")
 const preview = document.querySelector(".img-upload__preview>img");
@@ -11,39 +10,60 @@ const formClose = document.getElementById("upload-cancel")
 const imgTextContainer = document.querySelector(".img-upload__text")
 
 hashtagsText.addEventListener("keyup", function(){
-  // аааа, с регуляркой не могу совладать
-  this.value = this.value.replace(/^#([a-zA-Za-яА-Я0-9]){1,20}/g, '');
+  
+  this.value = this.value.replace(/[^#a-zA-Zа-яА-Я0-9]+ $/g, '');
 })
  
 hashtagsText.addEventListener("change", validForm)
+ 
+const p = document.createElement("p")
+imgTextContainer.append(p) 
 
-function validForm(evt){
-  let array  = hashtagsText.value.split(" ");
-    for(item of array){
-      if(item[0] !== "#"){
-        hashtagsText.classList.add("invalid")
-      }
-      if(item.lenght == 1){
-        const span = document.createElement("span")
-        const text = "Мин. длинна - 1 символ, не включая решетку";
-        span.innerText = text
-        imgTextContainer.append(span)
-      }
-      if(item.lenght > 20){
-        const span = document.createElement("span")
-        const text = "Слишком большой, до 20 символов, без решетки";
-        span.innerText = text
-        imgTextContainer.append(span)
-      }
+
+function validForm(){
+  debugger
+ 
+    let arrayString = hashtagsText.value.split(" ")
+    if(arrayString.length > 5){
+      hashtagsText.classList.add("invalid") 
+      const text = "Не больше 5-ти хэш-тегов";
+      p.innerText = text 
     }
+    for( let item of arrayString){
+      if(item[0] !== "#"){     
+        hashtagsText.classList.add("invalid")    
+        const text = "Отсутствует знак #";
+        p.innerText = text     
+      }
+      if(item.length < 2){
+        hashtagsText.classList.add("invalid")
+        const text = "Мин. длинна - 1 символ, не включая решетку";
+        p.innerText = text
+        
+      }
+      if (item.indexOf("#", 1) > 0) {
+        hashtagsText.classList.add("invalid")
+        const text = "Хэш-теги нужно разделить пробелами";
+        p.innerText = text       
+      
+      }
+      else if(item.length > 20){
+        hashtagsText.classList.add("invalid")
+        const text = "Слишком большой, до 20 символов";
+        p.innerText = text
+        
+      }else {
+        hashtagsText.classList.add("valid")
+        hashtagsText.classList.remove("invalid")
+      }
+    } 
 }
 
 
 uploadFile.addEventListener("change", open)
 
 export function open(evt){
- 
-  debugger
+
   let sellectedFile = evt.target.files[0];
   let reader= new FileReader();
   reader.onload =function(evt){
